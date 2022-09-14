87ab76b32783cc9e8681c1100c082357e541d6f0
JesusCervantesBalderama <jose.jesus.cervantes.balderrama@uabc.edu.mx>
2022-09-13 18:31:27 -0700

    Agregar archivos de proyecto.

---
+++ Practica 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 23;
+            string nombre = "Jesus";
+            string pais = "Mexico";
+
+
+            Console.WriteLine("La edad que tengo son {0}, mi nombre es {1}, y el pais donde vivo y naci es {2} ", edad,nombre,pais);
+            Console.ReadLine();
+
+        }
+    }
+}
